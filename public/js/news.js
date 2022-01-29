a159b453240989d752f78b712fd633afab69a830
Pedro Valente <valentedev09@gmail.com>
2022-01-28 22:26:06 -0300

    implementação visual notícias, botão 'ler mais'

--- public/js/news.js
+++ public/js/news.js
@@ -1,1 +1,14 @@
+const p = document.getElementsByTagName("p")
+for (let p_ of p) {
+    let pValue = p_.innerHTML
+    let pLastChar = pValue.charAt(pValue.length - 1)
+    while (pLastChar == ' ' ||
+        pLastChar == ',' ||
+        (pValue.length > 60)) {
+        pValue = pValue.slice(0, -1)
+        pLastChar = pValue.charAt(pValue.length - 1)
+    }
+    console.log(pValue);
+    p_.innerHTML = pValue + '...'
+}
 
