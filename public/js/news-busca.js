fef9a495aa84328de86d9b44a022d6893dd28763
Pedro Valente <valentedev09@gmail.com>
2022-02-01 21:17:44 -0300

    atualizações no responsivo

--- public/js/news-busca.js
+++ public/js/news-busca.js
@@ -1,9 +1,14 @@
 const btn = document.getElementsByClassName('busca-btn')[0]
 const busca = document.getElementsByClassName('news-busca')[0]
 
-console.log(btn);
-
 btn.onclick = function () {
-    console.log('aaaaa');
     busca.classList.toggle('busca-open')
+}
+
+const newsMenu = document.getElementsByClassName('news-menu')[0]
+const mobileNav = document.getElementsByClassName('mobile-nav')[0]
+
+newsMenu.onclick = function () {
+    mobileNav.classList.toggle('mobile-nav-hide')
+    newsMenu.classList.toggle('change')
 }
